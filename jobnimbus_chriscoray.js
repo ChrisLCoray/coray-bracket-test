6251bc8595861ecfc6599d302afa853fea7e0aef
Chris <chris.coray@gmail.com>
2021-08-10 10:37:28 -0600

    converted function to method

--- jobnimbus_chriscoray.js
+++ jobnimbus_chriscoray.js
@@ -1,25 +1,25 @@
 /**
  * JobNimbus Technical test
  * Chris Coray
- * 
+ *
  * Problem 1:
  * Write a method that takes a string argument and returns whether or not characters in the string have matching brackets.
- * Meaning for every { there is a corresponding } bracket. 
- * Return true if they do, return false if they do not. 
- * 
+ * Meaning for every { there is a corresponding } bracket.
+ * Return true if they do, return false if they do not.
+ *
  * Please unit test with the following use cases (and any others you choose to ensure functionality):
- * 
+ *
  * Test string - Expected result:
- * 
+ *
  * {} - True
  * }{ - False (closed bracket can't proceed all open brackets.)
  * {{} - False (one bracket pair was not closed)
  * "" - True. (no brackets in the string will return True)
- * 
+ *
  **/
 
 (function () {
-    function testStringBrackets(testString) {
+    const testStringBrackets = (testString) => {
         var brokenBracket = 0;
         var openBracket = -1;
 
